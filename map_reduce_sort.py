749e39d67efc112102502c46cb9bf23147a3754c
Prakhar Gandhi <gprakhar0@gmail.com>
2026-07-26 10:35:00 +0530

    Update map_reduce_sort.py

--- map_reduce_sort.py
+++ map_reduce_sort.py
@@ -28,13 +28,27 @@ def map_reduce_sort(array: np.ndarray, partition_count: int) -> np.ndarray:
     )
 
 
-def optimized_scalable_sort(N,partition_count):
-    np_array = map_reduce_sort(np.random.rand(N),partition_count)
-    return {"np_array":np_array,"np_array_size":len(np_array)}
+def optimized_scalable_sort(np_array, partition_count):
+    np_array = np_array/len(np_array)
+    np_array_sorted = map_reduce_sort(np_array, partition_count)*len(np_array)
+    return {"np_array_sorted":np_array_sorted,"np_array_size":len(np_array)}
 
-# if __name__=="__main__":
-#     N = int(pow(10,8))
-#     partition_count = 4
-#     array_data_dict = optimized_scalable_sort(N,partition_count)
-#     from pprint import pprint
-#     pprint(array_data_dict)
+def optimized_scalable_topk_elements_getter(np_array, partition_count, top_k):
+    np_array = np_array/len(np_array)
+    np_array_sorted = (map_reduce_sort(np_array, partition_count)*len(np_array))[:top_k]
+    return {"top_k_elements":np_array_sorted,"top_k":len(np_array_sorted)}
+
+    
+
+if __name__=="__main__":
+    N = int(pow(10,8))
+    partition_count = 4
+    arr = [i for i in range(N)]
+    np_array = np.array(arr)
+    array_data_dict = optimized_scalable_sort(np_array, partition_count)
+    from pprint import pprint
+    pprint(array_data_dict)
+
+    top_k = 100
+    array_data_dict = optimized_scalable_topk_elements_getter(np_array, partition_count, top_k)
+    pprint(array_data_dict)
